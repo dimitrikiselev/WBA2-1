d592a5db25dec7f39da6e2d5fed00f4f429103b6
Jörn Fiege <joern.fiege@web.de>
2013-04-13 19:33:30 +0200

    neu

--- WBA_Phase1/src/Aufgabe4.java
+++ WBA_Phase1/src/Aufgabe4.java
@@ -17,18 +17,23 @@ public class Aufgabe4 {
 		
 		Scanner in = new Scanner(System.in);
 		
-
-			System.out.println("Wählen Sie die Nummer Ihres gewünschten Rezeptes.");
-				auflistung();
-	
-				int a = in.nextInt();
-				int index;
-				switch(a){
+			for(int i=0; i<1; i++){
+				System.out.println("Wählen Sie die Nummer Ihres gewünschten Rezeptes.");
+					auflistung();
+		
+					int a = in.nextInt();
+						infos(a); 
+						kommentare(a);
+				System.out.println("\n\n");
+				System.out.println("Wenn sie Kommetare hinzufügen wollen, wählen sie: \t 1");
+				System.out.println("Wenn Sie ein andere Rezept wählen wollen wählen sie: \t 2");
+				System.out.println("Wenn Sie das Programm beenden wollen wählen sie: \t 3");
+					
+					int tmp = in.nextInt();
+				if(tmp == 2) i = 0;
+				if(tmp == 3) ; //?
+			}	
 				
-					case 0:
-						infos(a); break;
-						
-				}
 	}
 				
 
@@ -42,21 +47,39 @@ private static void auflistung() throws JAXBException{
 }
 
 private static void infos(int i) throws JAXBException{
-		Rezept r = getRezepte().getRezept().get(i);
 		
-			System.out.println(getRezepte().getRezept().get(i).getRezeptname() + "\n");
+	Rezept r = getRezepte().getRezept().get(i);
 		
-			System.out.println("Bild Nummer" + r.getBilder().getBild().get(0).getBildId());
-			System.out.println(r.getBilder().getBild().get(0).getLink());
-			System.out.println("Gepostet von:" + r.getBilder().getBild().get(0).getUser() + "\n");
+		System.out.println(getRezepte().getRezept().get(i).getRezeptname() + "\n");
 		
-			int b = 1;
-			for(int a = 0; a < r.getZutaten().getZutat().size(); a++, b++)
-			System.out.println("Zutat " + b + " ist: " + r.getZutaten().getZutat().get(a).getMenge() + r.getZutaten().getZutat().get(a).getEinheit() + r.getZutaten().getZutat().get(a).getName());
+		System.out.println("Bild Nummer" + r.getBilder().getBild().get(0).getBildId());
+		System.out.println(r.getBilder().getBild().get(0).getLink());
+		System.out.println("Gepostet von:" + r.getBilder().getBild().get(0).getUser() + "\n");
+		
+		int b = 1;
+		for(int a = 0; a < r.getZutaten().getZutat().size(); a++, b++){
+			System.out.println("Zutat " + b + " ist: " + r.getZutaten().getZutat().get(a).getMenge() + " " + r.getZutaten().getZutat().get(a).getEinheit() + " " + r.getZutaten().getZutat().get(a).getName());
+		}
 			
+		System.out.println("\n" + "Arbeitszeit: " + r.getZubereitung().getZeit());
+		System.out.println("Schwierigkeitsgrad: " + r.getZubereitung().getSchwierigkeit());
+		System.out.println("Brennwert: " + r.getZubereitung().getBrennwert());
+		System.out.println("Zubereitung: " + r.getZubereitung().getAnleitung() + "\n\n");
 }
 	
+private static void kommentare(int i) throws JAXBException{
 	
+	Rezept r = getRezepte().getRezept().get(i);
+	
+		System.out.println("Kommentare: \n");
+		int b = 1;
+		for(int a = 0; a < r.getComments().getComment().size(); a++, b++){
+			System.out.println("Kommentar " + b + ", von " + r.getComments().getComment().get(a).getUser() + " am " + r.getComments().getComment().get(a).getDatum());
+			System.out.println(r.getComments().getComment().get(a).getUserPic());
+			System.out.println(r.getComments().getComment().get(a).getKommentar() + "\n");
+		}
+}
+
 
 		
 private static Rezepte getRezepte() throws JAXBException {
